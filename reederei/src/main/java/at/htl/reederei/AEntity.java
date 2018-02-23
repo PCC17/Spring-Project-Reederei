04f17042681ca621bb126e770405e5de3f0a96ad
Gabriel Naber <g.naber@htlkrems.at>
2018-02-23 14:11:23 +0100

    commit

---
+++ reederei/src/main/java/at/htl/reederei/AEntity.java
@@ -0,0 +1,11 @@
+package at.htl.reederei;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class AEntity implements Serializable {
+
+
+}
---
+++ reederei/src/main/java/at/htl/reederei/ReedereiApplication.java
@@ -0,0 +1,12 @@
+package at.htl.reederei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReedereiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReedereiApplication.class, args);
+	}
+}
